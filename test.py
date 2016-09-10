31eabe12aba13800449f11591a4dde3eb168fc21
Clint-Holt <clinton.m.holt@vanderbilt.edu>
2016-09-10 17:12:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ def test(n):
   """ Test to see if writing files within GitHub works."""
   print 'This test worked.' * n
   
-def Main():
+def main():
   test(3)
   print 'Done'
   
-if __main__ == '__Main__':
+if __name__ == '__main__':
   main()
   
