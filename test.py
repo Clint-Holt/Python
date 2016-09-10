930aba4ecc4cd506f8008b17ec8ddd9e63d77046
Clint-Holt <clinton.m.holt@vanderbilt.edu>
2016-09-10 17:04:38 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+def test(n):
+  """ Test to see if writing files within GitHub works."""
+  print 'This test worked.' * n
+  
+def main():
+  test(3)
+  print 'Done'
+  
+if __main__ == 'main':
+  main()
+  
